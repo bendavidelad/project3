7890d1621f3c0812636a9f55e903ecbe45b8d700
shaked571 <shakedgreenfeld@gmail.com>
2017-05-06 19:32:35 +0300

    sss

--- MapReduceClientUser.cpp
+++ MapReduceClientUser.cpp
@@ -4,21 +4,34 @@
 #include <iostream>
 #include "MapReduceClient.h"
 
-class FolderNameKey :public k1Base{
-    FolderNameKey(std::string name):folderName(name)
+class SubStringKey :public k1Base{
+    SubStringKey(std::string name):subString(name)
     {}
 
     ~FolderNameKey(){}
 
-    std::string getFolerName(){
-        return folderName;//ddd
+    std::string getSubString(){
+        return subString;
     }
     bool operator<(const k1Base &other) const {
-        FolderNameKey otherIns = (const FolderNameKey&) other;
+        SubStringKey otherIns = (const SubStringKey&) other;
 
-        return this->folderName < otherIns.getFolerName();
+        return this->subString < otherIns.getSubString();
     }
 
 private:
+    std::string subString;
+};
+
+
+class FolderNameKey: public v1Base{
+    FolderNameKey(std::string name): folderName(name)
+    {}
+    std::string getFoloderName(){
+        return folderName;
+    }
+    
+private:
     std::string folderName;
+
 };
